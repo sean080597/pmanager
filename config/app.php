b69f2ee58cf9a05d9717d56cc84e75265712337e
Luu Sean <cuong.luusean@gmail.com>
2018-06-20 01:44:50 +0700

    Created some table

--- config/app.php
+++ config/app.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_PNAME', 'Laravel'),
+    'title' => 'First Laravel 5 App',
 
     /*
     |--------------------------------------------------------------------------
